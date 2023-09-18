3d7e0ded685e9250972f276aea0062d7fc9e8a41
王小汪 <wangxy98c@qq.com>
2023-09-18 20:44:58 +0800

    添加了SpringBootTest的依赖以及pom的配置

---
+++ tienchin-admin/src/test/java/com/wangxy/tienchin/GenerateCode.java
@@ -0,0 +1,12 @@
+package com.wangxy.tienchin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GenerateCode {
+    @Test
+    void generateChannelCode(){
+        
+    }
+}
